b7e2486dfecdfa2681661075513d64fdc926351c
Gerrit Wanderer <gerrit@the-wanderer.de>
2012-07-17 21:42:33 +0200

    add rails application

---
+++ rails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dogpots::Application.config.secret_token = 'bd32e88a94667a75a375b6314369b087648a9924e8ac1abd58fc308abe38a29417216161934aea5e717708ddf4223d9227c7d6f68e2691a2bd2eef9d44fce087'
