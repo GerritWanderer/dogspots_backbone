ed3fccafcadbe34eb8c3e43f033b4b11ef400299
Gerrit Wanderer <wanderwelten+account@googlemail.com>
2012-08-02 16:34:24 +0200

    Install and setup rails_admin

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,4 +1,7 @@
 Dogspots::Application.routes.draw do
+  mount RailsAdmin::Engine => '/danger', :as => 'rails_admin'
+  devise_for :admins
+
   resources :spots, except: :edit, :defaults => { :format => 'json' }
   resources :comments, except: :edit, :defaults => { :format => 'json' }
 
--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120802142231) do
+ActiveRecord::Schema.define(:version => 20120802142810) do
 
   create_table "admins", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -41,6 +41,19 @@ ActiveRecord::Schema.define(:version => 20120802142231) do
 
   add_index "comments", ["spot_id"], :name => "index_comments_on_spot_id"
   add_index "comments", ["user_id"], :name => "index_comments_on_user_id"
+
+  create_table "rails_admin_histories", :force => true do |t|
+    t.text     "message"
+    t.string   "username"
+    t.integer  "item"
+    t.string   "table"
+    t.integer  "month",      :limit => 2
+    t.integer  "year",       :limit => 5
+    t.datetime "created_at",              :null => false
+    t.datetime "updated_at",              :null => false
+  end
+
+  add_index "rails_admin_histories", ["item", "table", "month", "year"], :name => "index_rails_admin_histories"
 
   create_table "ratings", :force => true do |t|
     t.integer  "spot_id"
